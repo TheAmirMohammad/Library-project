864164debac1a439b7e2de581c8328c894db179f
= <=>
2021-06-01 18:05:34 +0430

    Login - signin - pay

--- Library Manager/MainWindow.xaml.cs
+++ Library Manager/MainWindow.xaml.cs
@@ -16,5 +16,10 @@ namespace Library_Manager
         {
             this.Close();
         }
+
+        private void drag(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
     }
 }
